4152dcc220fe66a854c452d7f1de70863346d441
yultrish <yultrish@gmail.com>
2023-08-03 10:49:41 +0000

    update

--- JavaScript/test.js
+++ JavaScript/test.js
@@ -2,20 +2,21 @@ console.log(" test our understanding of data types, data properties, and data me
 
 // create an object
 let maxSpeed = {
-    car: 300, 
+    airplane: 1000,
+    car: 300,
+    motorbike: 200,  
     bike: 60, 
     motorbike: 200, 
-    airplane: 1000,
     helicopter: 400, 
 };
 
 // turn that object created above into an array
 maxSpeedArray = Object.entries(maxSpeed)
-console.log(maxArray)
+console.log(maxSpeedArray)
 
 //sort the array and console.log(results)
-maxSpeedArraysorted = maxArray.sort((a,b)=>{
-    return a[1] - b[1]
+maxSpeedArraysorted = maxSpeedArray.sort((a,b)=>{
+    return a[0] - b[0]
 })
 console.log(maxSpeedArraysorted)
 
